35fdedf6888d9b46a2700df87ee204f13cf029d9
LeoTeiSil <leoteixeira3010@gmail.com>
2026-06-09 15:00:14 -0300

    feat(aula03): add de implementar novo campo de estado

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ class ComponenteSchema(BaseModel):
     nome: str = Field(..., min_length=2, description="Nome do componente maker")
     quantidade: int = Field(..., ge=0, description="Quantidade em estoque (deve ser maior ou igual a zero)")
     categoria: str = Field(..., description="Categoria do item (ex: Atuadores, Microcontroladores)")
+    estado_conservacao: str = Field(..., min_length=1, description="Estado de conservação do componente (ex: Novo, Bom, Usado)")
 
 # 3. Nosso "Banco de Dados" temporário em memória
 estoque_laboratorio = [
-    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão"},
-    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores"},
-    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica"}
+    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão", "Estado": "Bom"},
+    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores", "Estado": "Novo"},
+    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica", "Estado": "Usado"}
 ]
 
 # Rota Raiz
@@ -26,6 +27,7 @@ def raiz():
 @app.get("/componentes")
 def listar_componentes():
     return estoque_laboratorio
+    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
 
 # CRUD - CREATE (Cadastrar novo item)
 @app.post("/componentes", status_code=201)
@@ -43,6 +45,7 @@ def adicionar_componente(novo_componente: ComponenteSchema):
     
     estoque_laboratorio.append(componente_dict)
     return {"mensagem": "Componente adicionado com sucesso!", "componente": componente_dict}
+    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
 
 # CRUD - UPDATE (Atualizar quantidade ou dados)
 @app.put("/componentes/{componente_id}")
@@ -52,9 +55,9 @@ def atualizar_componente(componente_id: int, dados_atualizados: ComponenteSchema
             item["nome"] = dados_atualizados.nome
             item["quantidade"] = dados_atualizados.quantidade
             item["categoria"] = dados_atualizados.categoria
+            item["estado_conservacao"] = dados_atualizados.estado_conservacao
             return {"mensagem": "Componente atualizado com sucesso!", "componente": item}
-            
-    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
+            raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
 
 # CRUD - DELETE (Remover item do inventário)
 @app.delete("/componentes/{componente_id}")
@@ -63,5 +66,4 @@ def remover_componente(componente_id: int):
         if item["id"] == componente_id:
             estoque_laboratorio.pop(index)
             return {"mensagem": f"Componente com ID {componente_id} foi removido do estoque."}
-            
-    raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
+            raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
